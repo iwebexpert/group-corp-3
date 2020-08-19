a2a061e6e68ca8dc86bf5a178c87a6da020399d2
denzLLL <alex.broad@mail.ru>
2020-08-19 22:43:57 +0300

    3

---
+++ kshirokiy/my-app/src/MessageField/MessageField.tsx
@@ -0,0 +1,55 @@
+import React, {useState, useEffect, useRef} from 'react';
+import Messages from '../Messages/Messages';
+
+type State = {
+    messages: Item[];
+};
+
+type Props = {};
+
+export const MessageField: React.FC<Props> = () => {
+    const [messages, setMessages] = useState<any[]>([]);
+    const inputMessageRef = useRef<HTMLInputElement>(null);
+    const inputAuthorRef = useRef<HTMLInputElement>(null);
+
+    const handleInputChange = (event: any) => {
+        console.log('event: ', event.target.value);
+    }
+
+    const handleButtonClick2 = (inputRef: any, inputAuthorRef: any) => {
+        let message = inputRef.current.value;
+        let author = inputAuthorRef.current.value;
+        inputRef.current.value = '';
+        inputAuthorRef.current.value = '';
+        if (message.trim() !== '' && author.trim() !== '') {
+            setMessages(currentState => ([...currentState, {message, author}]));
+        }
+    }
+
+    return (
+        <>
+            <Messages items={messages}></Messages>
+            <div>
+                <div>
+                    <input type="text"
+                           ref={inputMessageRef}
+                           name="message"
+                           placeholder="Введите сообщение"
+                           onChange={handleInputChange}/>
+
+                </div>
+                <div>
+                    <input type="text"
+                           ref={inputAuthorRef}
+                           name="message"
+                           placeholder="Введите Автора сообщение"
+                           onChange={handleInputChange}/>
+
+                </div>
+                <button onClick={() => {handleButtonClick2(inputMessageRef, inputAuthorRef); }}>
+                    Добавить сообщение
+                </button>
+            </div>
+        </>
+    );
+}
