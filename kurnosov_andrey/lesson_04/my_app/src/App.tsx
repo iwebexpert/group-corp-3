834227dbe7c1e2250afdc74ef0dacad18be19a33
Kurnosov Andrey <dryush97@gmail.com>
2020-08-23 21:44:40 +0400

    #Урок 4. Скопировал урок 3

---
+++ kurnosov_andrey/lesson_04/my_app/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import React from 'react';
+import { Chat } from './components/Chat/Chat';
+
+function App() {
+  return <>
+      <Chat></Chat>
+    </>;
+}
+
+export default App;
