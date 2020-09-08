43d20cdc1e850a0f3609df5fecb31040a7ee79b2
Boris Goncharov <thegoncharoffs@gmail.com>
2020-09-08 16:15:49 +0300

    Added formik and yup

--- BorisGoncharov/task8/src/components/SettingsModal/SettingsModal.tsx
+++ BorisGoncharov/task8/src/components/SettingsModal/SettingsModal.tsx
@@ -1,9 +1,20 @@
-import React, { FC, useState } from 'react';
-import { Button, Form, Modal } from 'react-bootstrap';
+import React, { FC } from 'react';
+import { Button, Modal, FormGroup, FormLabel } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { useFormField } from '../../hooks/useFormField';
 import './SettingsModal.scss';
 import classNames from 'classnames';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+
+const SettingsSchema = Yup.object().shape({
+  author: Yup.string()
+    .min(3, 'SHORT_TEXT_ERROR')
+    .max(30, 'LONG_TEXT_ERROR')
+    .matches(/^[a-zA-Zа-яА-ЯёЁ]+$/, 'PATTERN_ERROR')
+    .required('REQUIRED_ERROR'),
+  theme: Yup.string().required('REQUIRED_ERROR'),
+  language: Yup.string().required('REQUIRED_ERROR'),
+});
 
 export type SettingsModalProps = {
   settings: Settings;
@@ -21,33 +32,25 @@ export const SettingsModal: FC<SettingsModalProps> = ({
   onSettingsChange,
 }) => {
   const { t } = useTranslation();
-  const authorField = useFormField(settings.name);
-  const themeField = useFormField(settings.theme);
-  const languageField = useFormField(settings.language);
 
-  const [validated, setValidated] = useState<boolean>(false);
-
-  const handleSubmit = (event: React.FormEvent<HTMLElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget as HTMLFormElement;
-    if (form.checkValidity()) {
-      onSettingsChange({
-        name: authorField.value,
-        theme: themeField.value,
-        language: languageField.value,
-      });
-      handleModalClose();
-    } else {
-      setValidated(true);
-    }
+  const handleSubmit = (values: {
+    author: string;
+    theme: Theme;
+    language: Language;
+  }) => {
+    handleModalClose();
+    onSettingsChange({
+      name: values.author,
+      theme: values.theme,
+      language: values.language,
+    });
   };
 
   const handleModalClose = () => {
-    setValidated(false);
     onSettingsModalClose();
   };
 
-  const classes = classNames({
+  const modalClasses = classNames({
     'bg-white': theme === 'light',
     'bg-dark text-white': theme === 'dark',
   });
@@ -59,66 +62,86 @@ export const SettingsModal: FC<SettingsModalProps> = ({
 
   return (
     <Modal show={visible} onHide={handleModalClose}>
-      <Modal.Header className={classes}>
+      <Modal.Header className={modalClasses}>
         <Modal.Title>{t('SETTINGS')}</Modal.Title>
       </Modal.Header>
-      <Modal.Body className={classes}>
-        <Form
-          noValidate
-          validated={validated}
-          method="POST"
-          onSubmit={handleSubmit}
-        >
-          <Form.Group controlId="author">
-            <Form.Label>{t('AUTHOR')}</Form.Label>
-            <Form.Control
-              type="text"
-              name="author"
-              placeholder="Input author's name"
-              required
-              minLength={3}
-              maxLength={30}
-              pattern="[a-zA-Zа-яА-ЯёЁ]+"
-              {...authorField}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t('INVALID_FIELD')}
-            </Form.Control.Feedback>
-            <Form.Text className={suggestionsClasses}>
-              {t('AUTHOR_SUBTEXT')}
-            </Form.Text>
-          </Form.Group>
 
-          <Form.Group controlId="theme">
-            <Form.Label>{t('THEME')}</Form.Label>
-            <Form.Control as="select" required {...themeField}>
-              <option value="light">{t('LIGHT')}</option>
-              <option value="dark">{t('DARK')}</option>
-            </Form.Control>
-            <Form.Control.Feedback type="invalid">
-              {t('INVALID_FIELD')}
-            </Form.Control.Feedback>
-            <Form.Text className={suggestionsClasses}>
-              {t('THEME_SUBTEXT')}
-            </Form.Text>
-          </Form.Group>
+      <Formik
+        initialValues={
+          {
+            author: settings.name,
+            theme: settings.theme,
+            language: settings.language,
+          } as any
+        }
+        validationSchema={SettingsSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched, getFieldProps }) => (
+          <Form>
+            <Modal.Body className={modalClasses}>
+              <FormGroup>
+                <FormLabel>{t('AUTHOR')}</FormLabel>
+                <Field
+                  className="form-control"
+                  name="author"
+                  placeholder={t('INPUT_AUTHORS_NAME')}
+                />
+                {errors.author && touched.author ? (
+                  <div className="invalid-feedback d-block">
+                    {t(errors.author)}
+                  </div>
+                ) : null}
+              </FormGroup>
 
-          <Form.Group controlId="language">
-            <Form.Label>{t('LANGUAGE')}</Form.Label>
-            <Form.Control as="select" required {...languageField}>
-              <option value="en">{t('ENGLISH')}</option>
-              <option value="ru">{t('RUSSIAN')}</option>
-            </Form.Control>
-            <Form.Text className={suggestionsClasses}>
-              {t('LANGUAGE_SUBTEXT')}
-            </Form.Text>
-          </Form.Group>
+              <FormGroup>
+                <FormLabel>{t('THEME')}</FormLabel>
+                <Field
+                  className="form-control"
+                  name="theme"
+                  placeholder={t('CHOOSE_THEME')}
+                  as="select"
+                >
+                  <option value="light">{t('LIGHT')}</option>
+                  <option value="dark">{t('DARK')}</option>
+                </Field>
+                {errors.theme && touched.theme ? (
+                  <div className="invalid-feedback d-block">
+                    {t(errors.theme)}
+                  </div>
+                ) : null}
+              </FormGroup>
 
-          <Button variant="warning" type="submit">
-            {t('APPLY')}
-          </Button>
-        </Form>
-      </Modal.Body>
+              <FormGroup controlId="language">
+                <FormLabel>{t('LANGUAGE')}</FormLabel>
+                <Field
+                  className="form-control"
+                  name="language"
+                  placeholder={t('CHOOSE_LANGUAGE')}
+                  as="select"
+                >
+                  <option value="en">{t('ENGLISH')}</option>
+                  <option value="ru">{t('RUSSIAN')}</option>
+                </Field>
+                {errors.language && touched.language ? (
+                  <div className="invalid-feedback d-block">
+                    {t(errors.language)}
+                  </div>
+                ) : null}
+              </FormGroup>
+            </Modal.Body>
+
+            <Modal.Footer className={modalClasses}>
+              <Button type="submit" variant="warning">
+                {t('SAVE')}
+              </Button>
+              <Button variant="secondary" onClick={onSettingsModalClose}>
+                {t('CLOSE')}
+              </Button>
+            </Modal.Footer>
+          </Form>
+        )}
+      </Formik>
     </Modal>
   );
 };
