9853b132281333e35d25af3c77e69e24408e1a18
Boris Goncharov <thegoncharoffs@gmail.com>
2020-09-17 11:33:02 +0300

    Blocks bot from sending several messages

--- BorisGoncharov/task10/src/state/middlewares/botResponse.ts
+++ BorisGoncharov/task10/src/state/middlewares/botResponse.ts
@@ -1,6 +1,7 @@
 import { Middleware } from 'redux';
 import { generate } from 'shortid';
 import { chatsResetTypingAuthor, chatsSetTypingAuthor, MessagesActionTypes, messagesAdd, MessagesAddAction } from '../actions';
+import { AppState } from '../store';
 
 const BOT = {
   id: '007213',
@@ -10,8 +11,10 @@ const BOT = {
 export const botResponseMiddleware: Middleware = store => next => action => {
   if (action.type === MessagesActionTypes.MESSAGES_ADD) {
     const { chatId, author } = (action as MessagesAddAction).payload;
+    const typingAuthor = (store.getState() as AppState).chats.chats.find(chat => chat.id === chatId)?.typingAuthor;
+    const botIsWriting = typingAuthor === BOT.name;
 
-    if (author.id !== BOT.id) {
+    if (author.id !== BOT.id && !botIsWriting) {
       store.dispatch(chatsSetTypingAuthor({ chatId, author: BOT.name }));
 
       setTimeout(() => {
