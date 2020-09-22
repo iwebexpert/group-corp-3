f6aa06f3a876ce363ccb0c6d0df049300dafd83d
Maxim Artamoshkin <zverit@gmail.com>
2020-09-22 23:05:40 +0300

    homework 12

---
+++ MArtamoshkin/lesson 12/my-app/src/components/ChatContainer/index.ts
@@ -0,0 +1,1 @@
+import { ChatContainer } from './ChatContainer';
---
+++ MArtamoshkin/lesson 12/my-app/src/components/ChatList/ChatList.tsx
@@ -0,0 +1,57 @@
+import React, { MouseEvent} from "react";
+import classNames from 'classnames';
+import { Image, Spinner } from 'react-bootstrap';
+
+import './ChatList.scss';
+import { NewChat } from "./NewChat/NewChat";
+import { AppState } from "../../reducers";
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { push } from "connected-react-router";
+import { chatsDelete } from "../../actions/chats/delete";
+
+const ChatList = (props: ChatListProps) => {
+    const { activeChatId, isLoading, isError } = props;
+    const { t } = useTranslation();
+    const chats = useSelector<AppState, Chat[]>((state: AppState) => state.chats.items);
+
+    const dispatch = useDispatch();
+
+    const handleDeleteChat = (e: MouseEvent<HTMLElement>, id: number): void => {
+        e.preventDefault();
+        e.stopPropagation()
+
+        dispatch(push('/chat'));
+        dispatch(chatsDelete(id));
+    };
+
+    const handleClickOnChat = (chatId: number) => {
+        dispatch(push(`/chat/${chatId}`));
+    }
+
+    return (<div className="d-flex justify-content-between flex-column h-100">
+        <div className="chat-list d-flex flex-column">
+            {chats.map((chat: Chat) => <div onClick={() => handleClickOnChat(chat.id)}  key={chat.id} className="active-chat">
+                <div className={classNames('chat-field', { 'active-chat': chat.id === activeChatId, 'not-fired': !chat.isFired })}>
+                    <div className="chat-people">
+                        <div className="avatar"><Image src={chat.author.avatar} alt="Avatar" rounded /></div>
+                        <div className="chat-ib">
+                            <h5>{chat.author.name}<span className="float-right"><span
+                                className="chat-date">{!!chat.messages.length && new Date(chat.messages[chat.messages.length - 1].date).toLocaleDateString('ru-RU')}</span><i className="fas ml-2 remove-chat fa-trash float-right" onClick={(e) => handleDeleteChat(e, chat.id)}></i></span></h5>
+                            {<p className="text-truncate chat-text">
+                                {chat.isTyping && <>{t('TYPING')}</>}
+                                {chat.messages.length && !chat.isTyping ? chat.messages[chat.messages.length - 1].text : ''}
+                            </p>}
+                        </div>
+                    </div>
+                </div>
+            </div>)
+            }
+            {isLoading && <Spinner className="d-flex align-self-center mt-2" animation="grow" variant="secondary" />}
+        </div>
+        {!isLoading && !isError && <NewChat />}
+    </div>
+    );
+};
+
+export { ChatList };
