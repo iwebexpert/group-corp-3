e4ba8358946bd540c69f581cef1fe0d15460a38d
Boris Goncharov <thegoncharoffs@gmail.com>
2020-09-28 15:47:00 +0300

    Added template for task 14

---
+++ BorisGoncharov/task14/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+require('dotenv').config();
+
+const devMode = process.env.NODE_ENV === 'development';
+
+const config = {
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      components: path.join(__dirname, 'src', 'components'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      titleApp: 'Chat App',
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? 'main.css' : '[name].[hash].css',
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
+
+module.exports = (env, argv) => {
+  // We can alter config here ...
+  return config;
+};
