ca6afc20cb6ed00bbeb5f5c9e0c7e507a1e487fb
Boris Goncharov <thegoncharoffs@gmail.com>
2020-09-29 10:49:29 +0300

    Webpack config hmr and fonts

--- BorisGoncharov/task14/src/index.tsx
+++ BorisGoncharov/task14/src/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import ChatContainer from './containers/ChatContainer';
 import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from './state/store';
 import { Route, Switch } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path={`/:chatId`} exact>
-          <ChatContainer></ChatContainer>
-        </Route>
-        <Route path={`*`}>
-          <ChatContainer></ChatContainer>
-        </Route>
-      </Switch>
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+const App: FC = () => {
+  return hot(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path={`/:chatId`} exact>
+            <ChatContainer></ChatContainer>
+          </Route>
+          <Route path={`*`}>
+            <ChatContainer></ChatContainer>
+          </Route>
+        </Switch>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- BorisGoncharov/task14/webpack.config.js
+++ BorisGoncharov/task14/webpack.config.js
@@ -2,68 +2,123 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 require('dotenv').config();
 
-const devMode = process.env.NODE_ENV === 'development';
+const config = (devMode) => {
+  return {
+    entry: path.join(__dirname, 'src', 'index.tsx'),
 
-const config = {
-  entry: path.join(__dirname, 'src', 'index.tsx'),
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    alias: {
-      components: path.join(__dirname, 'src', 'components'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          react: {
+            automaticNamePrefix: 'react',
+          },
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
           },
         },
       },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
+    },
+
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+        components: path.join(__dirname, 'src', 'components'),
       },
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-typescript',
+                '@babel/preset-env',
+                '@babel/preset-react',
+              ],
+              plugins: [
+                '@babel/plugin-proposal-class-properties',
+                'react-hot-loader/babel',
+              ],
+            },
+          },
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { hmr: true, reloadAll: true }, // For HMR
+            },
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader', // Transforms image to Base64 and embeds ti bundle
+              options: {
+                limit: 8192, // Limits image size
+
+                fallback: 'file-loader', // Use file loader if image is big
+                name: devMode ? '[path][name].[ext]' : '[contenthash].[ext]',
+                outputPath: 'images', // Use file loader if image is big
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ttf|otf|woff|woff2)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html'),
+        filename: 'index.html',
+        titleApp: 'Chat App',
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+      new Dotenv(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-      titleApp: 'Chat App',
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? 'main.css' : '[name].[hash].css',
-    }),
-    new Dotenv(),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
+
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      compress: true,
+      publicPath: '/',
+      contentBase: path.join(__dirname, 'src'),
+    },
+  };
 };
 
 module.exports = (env, argv) => {
-  // We can alter config here ...
-  return config;
+  return config(argv.mode === 'development');
 };
